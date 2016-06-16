9a0e989f7bf4b99bceb5aa16da2888f2b6090459
stephane chretien <stephanechretien@stephanes-MacBook-Air.local>
2016-06-16 06:39:15 +0200

    fix multiple trace printing

--- index.js
+++ index.js
@@ -8,29 +8,9 @@ let PrettyStream = require('bunyan-prettystream');
 let prettyStdOut = new PrettyStream();
 prettyStdOut.pipe(process.stdout);
 
-let _createLogger = bunyan.createLogger;
+var _createLogger = bunyan.createLogger;
 bunyan.createLogger = function(options) {
     options.streams = [{
-        level: 'debug',
-        type: 'raw',
-        stream: prettyStdOut
-    }, {
-        level: 'info',
-        type: 'raw',
-        stream: prettyStdOut
-    }, {
-        level: 'warn',
-        type: 'raw',
-        stream: prettyStdOut
-    }, {
-        level: 'error',
-        type: 'raw',
-        stream: prettyStdOut
-    }, {
-        level: 'fatal',
-        type: 'raw',
-        stream: prettyStdOut
-    }, {
         level: 'trace',
         type: 'raw',
         stream: prettyStdOut
@@ -38,5 +18,6 @@ bunyan.createLogger = function(options) {
 
     return _createLogger(options);
 };
+
 
 module.exports = mocha.reporters.nyan;
